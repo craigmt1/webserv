7596f1abd9dfabee36b260419e6e65cf26a5aa28
craigmt1 <craigmt1@gmail.com>
2015-12-10 06:30:31 +0000

    added cgi script execution

--- webserv.c
+++ webserv.c
@@ -13,24 +13,28 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-
 //http://blog.manula.org/2011/05/writing-simple-web-server-in-c.html
 //http://shoe.bocks.com/net/
 //http://www.tldp.org/LDP/LGNET/91/misc/tranter/server.c.txt
 //http://stackoverflow.com/questions/10072989/simple-http-server-in-c-multiple-process-not-work-properly
 
-int port_no; //port number for webserv
-
-int serv_sockfd; //server socket file descriptor
-size_t serv_sock_size; //size of server socket file descriptor
-
 struct sockaddr_in addr; //server socket address structure
 socklen_t addrlen; //address structure size
 
-void exithandler();
+static const char site_directory[128] = "./www/"; //path of site resources
+static const char error_directory[128] = "./www/err/"; //path of error code html files
+
+static int serv_sockfd; //server socket file descriptor
+static size_t serv_sock_size; //size of server socket file descriptor
+static int port_no; //port number for webserv
+static const int req_buf_size = 1024; //client request buffer size
+
 void client_request(int client_sockfd);
 void write_file(int client_sockfd, char *req_path, int filesize);
 void write_dir(int client_sockfd, char *req_path);
+void write_cgi(int client_sockfd, char *req_path);
+void write_error(int client_sockfd, int error_code);
+void exithandler();
 
 int main(int argc, char **argv){
     //check arguments
@@ -60,7 +64,7 @@ int main(int argc, char **argv){
     signal(SIGQUIT, exithandler);
     printf("Successfully bound socket %d to port %d.  Press (Ctrl-C) to exit.\nListening...\n", serv_sockfd, port_no);
 
-    chdir("./www/"); //change working directory to website directory
+    chdir(site_directory); //change working directory to website directory
 
     //begin listen loop
     while(1) {
@@ -89,7 +93,6 @@ int main(int argc, char **argv){
 //handle single request from a given client socket
 void client_request(int client_sockfd){
     //initialize request buffer
-    int req_buf_size = 1024;
 	char req_buf[req_buf_size];
 
     //load messages from client_sockfd into request buffer http://linux.die.net/man/2/recv
@@ -97,34 +100,25 @@ void client_request(int client_sockfd){
     printf("Request from client socket %d\n%s", client_sockfd, req_buf); //output request message to console
     
     //tokenize request buffer (first token is request type)
-    char * req_tokens;
-    req_tokens = strtok(req_buf, " ");
+    char * req_tokens = strtok(req_buf, " ");
     
     //close if not GET request
     if (strcmp(req_tokens, "GET") != 0){
-    	printf("Expected GET, received: %s\n", req_tokens);
-    	//write_error(client_sockfd, 400);
+    	printf("ERROR: Expected GET, received: %s\n", req_tokens);
+    	write_error(client_sockfd, 404);
     	return;
     }
 
     //store next token (PATH), redirect to index.html if root dir
     req_tokens = strtok(NULL, " ");
     char req_path[256] = ".";
-    if (strcmp(req_tokens, "/") == 0) strcpy(req_path + 1, "/index.html");
-    else strcpy(req_path + 1, req_tokens);
-
-    //clear weird characters
-    fflush(stdout);
-    printf("PAGE REQUESTED: %s\n", req_path);
+    if (strcmp(req_tokens, "/") == 0) strcat(req_path, "/index.html");
+    else strcat(req_path + 1, req_tokens);
     
 	struct stat sb;
-	if (stat(req_path, &sb) < 0) {
-		perror("checkPath: stat()");
-		strcpy(req_path + 1, "/404.html");
-		//write_error(client_sockfd, 404);
-		//return;
-		}
+	if (stat(req_path, &sb) < 0) perror("checkPath: stat()");
 
+    printf("PAGE REQUESTED: %s\n", req_path);
 	switch (sb.st_mode & S_IFMT) {
 		case S_IFDIR:
 			write_dir(client_sockfd, req_path);
@@ -133,54 +127,90 @@ void client_request(int client_sockfd){
 			write_file(client_sockfd, req_path, (int) sb.st_size);
 			break;
 		default:
-			printf("INVALID: %s\n", req_path);
+			write_error(client_sockfd, 404);
 			break;
 	}
-
-	return;
-	/*
-    //create file descriptor (default: no page specified, return index.html)
-    int isIndex;
-    if (strcmp(req_path, "./") == 0) {
-        req_fd = open("./index.html", O_RDONLY);
-        isIndex = 1;
-    }
-    else {
-        req_fd = open(req_path, O_RDONLY);
-        isIndex = 0;
-    }
-
-    //404 file not found
-    int is404 = 0;
-    if (req_fd < 0) {
-        write(client_sockfd, "HTTP/1.0 404 Not Found\n", 23);
-        req_fd = open("./404.html", O_RDONLY);
-        is404 = 1;
-    } else write(client_sockfd, "HTTP/1.1 200 OK\n", 16);
-
-    //check if file or directory
-    struct stat sb;
-    fstat(req_fd, &sb);
-    //printf("STAT TYPE: %d\tFILE?: %d\tDIR?: %d\n", sb.st_mode, S_ISREG(sb.st_mode), S_ISDIR(sb.st_mode));
-
-    //for directory, generate directory listing
-    if (S_ISDIR(sb.st_mode) && !isIndex) {
-		write_dir(client_sockfd, req_fd, req_path);
-        close(req_fd); //close file
-        return;
-    }
-    //otherwise assume file
-    */
     return;
 }
 
+//parses error code, sends corresonding error code in /err/ directory if it exists using write_file
+void write_error(int client_sockfd, int error_code){
+	chdir("..");
+	chdir(error_directory);
+	char err_path[128] = "./";
+    sprintf(err_path + 2, "%d", error_code);
+    strcat(err_path, ".html");
+    
+    struct stat sb;
+    if (stat(err_path, &sb) < 0) {
+    	perror("ErrorHtmlFile: stat()");
+    	return;
+    } else write_file(client_sockfd, err_path, (int) sb.st_size);
+    return;
+}
+
+void write_cgi(int client_sockfd, char *req_path){
+	FILE *f;
+
+	printf("EXECUTING CGI SCRIPT: %s\n", req_path);
+	if ((f = popen(req_path, "r")) < 0){
+		perror("cgi:popen()");
+		return;
+	}
+	//write(client_sockfd, "Content-length: 4096\n", 21);
+	//write(client_sockfd, "Connection: close\n", 18);
+	//write(client_sockfd, "Content-Type: text/plain\n", 25);
+	//write(client_sockfd, "\nContent-Type: text/html\n\n", 26);
+	//write(client_sockfd, "HELLO\n", 6);
+
+	//write stdout line by line
+	char line[256];
+	//char cgi_out[10000];
+	while (fgets(line, 256, f) != NULL){
+		//strcat(cgi_out, line);
+		//printf("%s\n", line);
+		write(client_sockfd, line, strlen(line));
+	}
+
+	//printf("CGI OUTPUT: %s\n", cgi_out);
+
+	/*
+	//write content length
+    char filesizestr[32];
+    sprintf(filesizestr, "%d", (int) strlen(cgi_out));
+    write(client_sockfd, "Content-length: ", 16);
+    write(client_sockfd, filesizestr, strlen(filesizestr));
+    write(client_sockfd, "\n", 1);
+
+    //write output
+    write(client_sockfd, cgi_out, strlen(cgi_out));
+	*/
+	pclose(f);
+	close(client_sockfd);
+	return;
+}
+
+//writes a file to the client socket using a string for filename and int for size of file
 void write_file(int client_sockfd, char *req_path, int filesize){
 	int req_fd; //file descriptor for requested page
-	printf("RETRIEVING FILE FOR:%s\tSIZE: %d\n", req_path, filesize);
-	
-	req_fd = open(req_path, O_RDONLY);
+	char *ext; //file extension of requested page
+
 	write(client_sockfd, "HTTP/1.1 200 OK\n", 16);
 
+	//get file extension
+	if ((ext = strrchr(req_path, '.')) > 0) printf("PAGE EXTENSION: %s\n", ext);
+
+	//redirect to write_cgi if cgi file
+	if (strcmp(ext, ".cgi") == 0) {
+		write_cgi(client_sockfd, req_path);
+		return;
+	}
+
+	printf("RETRIEVING FILE FOR:%s\tSIZE: %d\n", req_path, filesize);
+	
+	//init file descriptor
+	req_fd = open(req_path, O_RDONLY);
+	
 	//content length string
     char filesizestr[32];
     sprintf(filesizestr, "%d", filesize);
@@ -188,9 +218,6 @@ void write_file(int client_sockfd, char *req_path, int filesize){
     write(client_sockfd, filesizestr, strlen(filesizestr));
     write(client_sockfd, "\n", 1);
 	
-    //get file extension for page request
-    char *ext;
-    if ((ext = strrchr(req_path, '.')) > 0) printf("PAGE EXTENSION: %s\n", ext);
     //examine extension and write file type
     if (strcmp(ext, ".html") == 0) write(client_sockfd, "Content-Type: text/html\n\n", 25);
     else if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) write(client_sockfd, "Content-Type: image/jpeg\n\n", 26);
@@ -220,7 +247,77 @@ void write_dir(int client_sockfd, char *req_path){
 	}
 	write(client_sockfd, "HTTP/1.1 200 OK\n", 16);
 
-	write(client_sockfd, "Content-length: 10000\n", 22);
+	char dirstr[10000] = "<html>\n\t<head>\n\t\t<title>Index of ";
+	strcat(dirstr, req_path + 1);
+	strcat(dirstr, "</title>\n\t</head>\n\t<body>\n\t\t<h1>Index of ");
+	strcat(dirstr, req_path + 1);
+	strcat(dirstr, "</h1>\n\t\t<table>\n\t\t\t<tr><th colspan=\"5\"><hr></th></tr>");
+	//parent link
+	strcat(dirstr, "\n\t\t\t<tr><td valign=\"top\"><img src=\"/icons/back.gif\" alt=\"[DIR]\"></td><td><a href=\"");
+	strcat(dirstr, req_path + 1);
+	strcat(dirstr, "../\">Parent Directory</a></td><td>&nbsp;</td></tr>");
+
+	//generate links for all files in directory. render files in directory to table
+	d = opendir(req_path);
+	while ((dir = readdir (d)) != NULL) {
+		//skip cwd and parent
+		if ((strcmp(dir->d_name, ".") == 0) || (strcmp(dir->d_name, "..") == 0)) continue;
+		
+		//render listing for other files/directories
+		strcat(dirstr, "\n\t\t\t<tr><td valign=\"top\"><img src=\"/icons/");
+
+		//choose icon for directory or file
+		if (dir->d_type == DT_DIR) strcat(dirstr, "folder.gif\" alt=\"[DIR]");
+		else strcat(dirstr, "text.gif\" alt=\"[TXT]");
+
+		strcat(dirstr, "\"></td><td><a href=\"");
+		strcat(dirstr, req_path + 1);
+		strcat(dirstr, dir->d_name);
+		strcat(dirstr, "\">");
+		strcat(dirstr, dir->d_name);
+		strcat(dirstr, "</a></td><td>&nbsp;</td></tr>");
+	}
+	strcat(dirstr, "\n\t\t\t<tr><th colspan=\"5\"><hr></th></tr>\n\t\t</table>\n\t\t<address>Custom CS410 Webserver</address>\n\t</body>\n</html>");
+
+	char dirstr_size[32];
+	sprintf(dirstr_size, "%d", (int) strlen(dirstr));
+	write(client_sockfd, "Content-length: ", 16);
+	write(client_sockfd, dirstr_size, strlen(dirstr_size));
+	write(client_sockfd, "\nContent-Type: text/html\n\n", 26);
+	write(client_sockfd, dirstr, strlen(dirstr));
+
+	closedir(d);
+	return;
+}
+
+void exithandler(){
+    //gracefully close socket file descriptor
+    printf("\nClosing socket %d on port %d...", serv_sockfd, port_no);
+    if (close(serv_sockfd) < 0){
+        printf(" Failure!\n");
+    }
+    else printf(" Success!\n");
+    exit(0);
+}
+
+
+/*
+//generate a directory listing for when a user navigates to directory
+//requires client socket id, requested directory file id, and specified path
+void write_dir(int client_sockfd, char *req_path){
+	DIR *d;
+	struct dirent *dir;
+	printf("GENERATING DIRECTORY LISTING FOR:%s\n", req_path);
+
+	//append slash to directory path string if it doesnt already exist
+	if (req_path[strlen(req_path) - 1] != '/'){
+		strcat(req_path, "/");
+		printf("NEW DIR STRING: %s\n", req_path);
+	}
+	write(client_sockfd, "HTTP/1.1 200 OK\n", 16);
+
+	//write(client_sockfd, "Content-length: 10000\n", 22);
+	write(client_sockfd, "Connection: close\n", 18);
 
 	write(client_sockfd, "Content-Type: text/html\n\n", 25);
 	write(client_sockfd, "<html>\n\t<head>\n\t\t<title>Index of ", 33);
@@ -255,6 +352,7 @@ void write_dir(int client_sockfd, char *req_path){
 		else write(client_sockfd, "text.gif\" alt=\"[TXT]", 21);
 
 		write(client_sockfd, "\"></td><td><a href=\"", 20);
+		write(client_sockfd, req_path + 1, strlen(req_path + 1));
 		write(client_sockfd, dir->d_name, strlen(dir->d_name));
 		write(client_sockfd, "\">", 2);
 		write(client_sockfd, dir->d_name, strlen(dir->d_name));
@@ -265,13 +363,4 @@ void write_dir(int client_sockfd, char *req_path){
 	write(client_sockfd, "\n\t\t\t<tr><th colspan=\"5\"><hr></th></tr>\n\t\t</table>\n\t\t<address>Custom CS410 Webserver</address>\n\t</body>\n</html>", 110);
 	return;
 }
-
-void exithandler(){
-    //gracefully close socket file descriptor
-    printf("\nClosing socket %d on port %d...", serv_sockfd, port_no);
-    if (close(serv_sockfd) < 0){
-        printf(" Failure!\n");
-    }
-    else printf(" Success!\n");
-    exit(0);
-}
+*/
